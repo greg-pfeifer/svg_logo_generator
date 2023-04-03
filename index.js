06feb68ba3f73585fb419a0a4249b73a8b956c07
Greg P <gpfeif20@gmail.com>
2023-04-03 17:28:49 -0400

    created init function with if/else stmts.

--- index.js
+++ index.js
@@ -24,3 +24,25 @@ const questions = [
         message: 'Please enter the desired color of the shape'
     },
 ]
+
+function writeToFile(filename, data) {
+    return fs.writeFile(filename, data, (err) => {
+        if(err)
+        return console.log(err)
+    })    
+}
+
+function init () {
+    inquirer.prompt(questions)
+        .then((questions) => {
+            if (questions.shape == 'circle') {
+                writeToFile('./logo.svg', generateCircle(questions))
+            } else if (questions.shape == 'polygon') {
+                writeToFile('./logo.svg', generatePolygon(questions))
+            } else if (questions.shape == 'rect') { 
+                writeToFile('./logo.svg', generateSquare(questions))
+            }
+        })
+}
+
+init()
